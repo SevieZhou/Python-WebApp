8c5f7e08033f537cdde65721c9327783af5b96ea
seviezhou <seviezhou@163.com>
2016-08-10 10:11:17 +0800

    add

--- www/app.py
+++ www/app.py
@@ -6,6 +6,26 @@ import asyncio, os, json, time
 from datetime import datetime
 
 from aiohttp import web
+from jinja2 import Environment, FileSystemLoader
+
+import orm
+from coroweb import add_routes, add_static
+
+def init_jinja2(app, **kw):
+	logging.info('init jinja2...')
+	options = dict(
+		autoescape = kw.get('autoescape', True),
+		block_start_string = kw.get('block_start_string', '{%'),
+		block_end_string = kw.get('block_end_string', '%}'),
+		variable_start_string = kw.get('variable_start_string', '{{'),
+		variable_end_string = kw.get('variable_end_string', '}}'),
+		auto_reload = kw.get('auto_reload', True)
+	)
+	path = kw.get('path', None)
+	if path is None:
+		path = os.path.join()
+	logging.info('set jinja2 template path: %s' % path)
+	env = Environment(loader=FileSystemLoader)
 
 def index(request):
 	return web.Response(body = b'<h1>Hello</h1>', headers = {'Content-Type':'text/html;charset = utf-8'})
